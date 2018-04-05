a910b5d3e46009d3414a218b865e025dba220df7
YouAG <toksiko@hotmail.fr>
2018-04-05 20:11:10 +0200

    creation du game

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function cacher(){
+	document.getElementById("coeur").style.display = "none";}
+
+function voir(){
+		document.getElementById("coeur").style.display = "block";
+		document.getElementById("bouton").style.display = "none";}
+
+function popup(){
+setTimeout(function(){alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")}, 10000);
+}
